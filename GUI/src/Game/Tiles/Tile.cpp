a8be34a2a8432cdc5fa364ea14eb32470fd914cf
justine <justine.loizel@gmail.com>
2024-06-04 22:11:36 +0200

    add(gui/feature/init_gui): assets for test

--- GUI/src/Game/Tiles/Tile.cpp
+++ GUI/src/Game/Tiles/Tile.cpp
@@ -7,3 +7,134 @@
 
 #include "Tile.hpp"
 
+Tile::Tile(sf::Vector2i pos, int q0, int q1, int q2, int q3, int q4, int q5, int q6)
+{
+    this->pos = pos;
+    this->q0 = q0;
+    this->q1 = q1;
+    this->q2 = q2;
+    this->q3 = q3;
+    this->q4 = q4;
+    this->q5 = q5;
+    this->q6 = q6;
+    this->qAll = q0 + q1 + q2 + q3 + q4 + q5 + q6;
+}
+
+void Tile::addItem(int item)
+{
+    switch (item) {
+        case 0:
+            this->q0++;
+            break;
+        case 1:
+            this->q1++;
+            break;
+        case 2:
+            this->q2++;
+            break;
+        case 3:
+            this->q3++;
+            break;
+        case 4:
+            this->q4++;
+            break;
+        case 5:
+            this->q5++;
+            break;
+        case 6:
+            this->q6++;
+            break;
+    }
+}
+
+void Tile::removeItem(int item)
+{
+    switch (item) {
+        case 0:
+            this->q0--;
+            break;
+        case 1:
+            this->q1--;
+            break;
+        case 2:
+            this->q2--;
+            break;
+        case 3:
+            this->q3--;
+            break;
+        case 4:
+            this->q4--;
+            break;
+        case 5:
+            this->q5--;
+            break;
+        case 6:
+            this->q6--;
+            break;
+    }
+}
+
+int Tile::getItemsQuantity(int item)
+{
+    switch (item) {
+        case 0:
+            return (this->q0);
+        case 1:
+            return (this->q1);
+        case 2:
+            return (this->q2);
+        case 3:
+            return (this->q3);
+        case 4:
+            return (this->q4);
+        case 5:
+            return (this->q5);
+        case 6:
+            return (this->q6);
+    }
+    return (0);
+}
+
+sf::Vector2i Tile::getPos()
+{
+    return (this->pos);
+}
+
+void Tile::setAllItemsQuantity(int q0, int q1, int q2, int q3, int q4, int q5, int q6)
+{
+    this->q0 = q0;
+    this->q1 = q1;
+    this->q2 = q2;
+    this->q3 = q3;
+    this->q4 = q4;
+    this->q5 = q5;
+    this->q6 = q6;
+    this->qAll = q0 + q1 + q2 + q3 + q4 + q5 + q6;
+}
+
+void Tile::setItemQuantity(int item, int quantity)
+{
+    switch (item) {
+        case 0:
+            this->q0 = quantity;
+            break;
+        case 1:
+            this->q1 = quantity;
+            break;
+        case 2:
+            this->q2 = quantity;
+            break;
+        case 3:
+            this->q3 = quantity;
+            break;
+        case 4:
+            this->q4 = quantity;
+            break;
+        case 5:
+            this->q5 = quantity;
+            break;
+        case 6:
+            this->q6 = quantity;
+            break;
+    }
+}
--- GUI/src/Game/Tiles/Tile.hpp
+++ GUI/src/Game/Tiles/Tile.hpp
@@ -16,12 +16,17 @@
 class Tile
 {
     public:
-        Tile();
+        Tile(sf::Vector2i pos = sf::Vector2i(0, 0), int q0 = 0, int q1 = 0, int q2 = 0, int q3 = 0, int q4 = 0, int q5 = 0, int q6 = 0);
         ~Tile() = default;
         void addItem(int item);
         void removeItem(int item);
         int getItemsQuantity(int item);
+        void setAllItemsQuantity(int q0, int q1, int q2, int q3, int q4, int q5, int q6);
+        void setItemQuantity(int item, int quantity);
+        sf::Vector2i getPos();
     private:
+        sf::Vector2i pos;
+        int qAll;
         int q0;
         int q1;
         int q2;
