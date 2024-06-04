fe7a01adcaa50cc4f452a328332cd6e50c41deba
maxence <maxence.largeot@epitech.eu>
2024-06-04 16:25:23 +0200

    refactor(ai/feature/init_ai): theory working Server class that handle connection and messages. Only close is missing

--- ai/main.py
+++ ai/main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
+from src import Server as Server
+
 if __name__ == '__main__':
+    server = Server.Server("127.0.0.1", 8581)
     print('Hello, AI!')
--- ai/src/Server.py
+++ ai/src/Server.py
@@ -6,31 +6,58 @@
 ##
 
 import socket
+import select
 
 class Server:
     def __init__(self, _host, _port):
-        host = _host
-        port = _port
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = _host
+        self.port = _port
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            self.connect()
+            self.connect_server()
+            self.s.setblocking(False)
         except:
-            raise Exception("Error while connecting at init try connect method.")
+            raise Exception("Server : Error while connecting at init try connect method.")
     
-    def setHost(self, _host):
+    def set_host(self, _host):
         self.host = _host
     
-    def setPort(self, _port):
-        self.port = _port;
+    def set_port(self, _port):
+        self.port = _port
     
-    def connect(self):
+    def connect_server(self):
         try:
+            print("Host : " + self.host + " Port : " + str(self.port))
             self.s.connect((self.host, self.port))
             print("Client connected succesfully.")
         except:
-            raise Exception("Error while connecting try changing port / host.")
+            raise Exception("Server : Error while connecting try changing port / host.")
         
     def send(self, command):
-        pass
-    
-    
+        try:
+            str_length = len(command)
+            for i in range(0, str_length, 1024):
+                mess_part = command[i:i + 1024]
+                self.s.sendall(mess_part)
+        except Exception:
+            raise Exception("Server : Error while sending message.")
+        
+    def recv(self) -> str:
+        response = ""
+        try:
+            while True:
+                response_part = self.s.recv(1024)
+                if (response_part):
+                    response += response_part
+                else:
+                    break
+            return response
+        except Exception:
+            raise Exception("Server : Error during recieve. Probably dev error.")
+        
+    def check_read(self) -> bool:
+        ready_to_read, _, _ = select.select([self.s], [], [], 1)
+        if ready_to_read:
+            return True
+        else:
+            return False
