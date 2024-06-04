b23c1e5368852f779df852519f25d569b7966966
3w3n <ewen.emeraud@epitech.eu>
2024-06-04 15:44:24 +0200

    feat(ai/feature/command):create class command

---
+++ ai/src/Command.py
@@ -0,0 +1,44 @@
+
+class Command:
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.commandSend = []
+
+
+    def mooveForward(self):
+        self.commandSend.append("Forward")
+
+    def mooveRight(self):
+        self.commandSend.append("Right")
+
+    def mooveLeft(self):
+        self.commandSend.append("Left")
+
+    def look(self):
+        self.commandSend.append("Look")
+
+    def inventory(self):
+        self.commandSend.append("Inventory")
+
+    def broadcast(self, message):
+        self.commandSend.append("Broadcast " + message)
+
+    def connectNumber(self, number):
+        self.commandSend.append("Connect_nbr " + number)
+
+    def fork(self):
+        self.commandSend.append("Fork")
+
+    def eject(self):
+        self.commandSend.append("Eject")
+
+    def takeObject(self, object):
+        self.commandSend.append("Take " + object)
+
+    def setObject(self, object):
+        self.commandSend.append("Set " + object)
+
+    def incantation(self):
+        self.commandSend.append("Incantation")
