ddc29263a8717a82e9d499c21503d0683afd46c7
maxence <maxence.largeot@epitech.eu>
2024-06-20 18:02:49 +0200

    refactor(ai/feature/queen): Rework classes to add Interface + Trantorian class & rework server message handling'

--- ai/main.py
+++ ai/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from ai.src.Server import Server
-from ai.src.FlagParser import FlagParser
-from ai.src.queen.Queen import Queen
+from src.Server import Server
+from src.FlagParser import FlagParser
+from src.queen.Queen import Queen
 import sys
 
 if __name__ == '__main__':
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     try:
         server = Server(parser.get_host(), parser.get_port())
         queen = Queen(server)
-        queen.set_welcome_data(server, parser)
+        queen.set_starting_data(parser)
         queen.run()
     except Exception as e:
         print(e, file=sys.stderr)
